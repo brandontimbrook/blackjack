d7381dd9fa17b98a53e3c6a0023b6d7e4042f409
Brandon Timbrook <brandontimbrook.dev@gmail.com>
2026-09-09 21:09:23 -0400

    Continuing scoring and Ace value logic

--- blackjack.py
+++ blackjack.py
@@ -3,14 +3,14 @@ import random
 deck = []
 
 def start():
-    suits = ["hearts", "diamonds", "spades", "clubs"]
-    ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+    suits = ["♥", "♦", "♠", "♣"]
+    ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
     for suit in suits:
         for rank in ranks:
             if rank.isdigit():
                 value = int(rank)
-            elif rank.strip().lower() == "ace":
+            elif rank.strip().lower() == "a":
                 value = 11
             else:
                 value = 10
@@ -28,6 +28,8 @@ while True:
 
     dealer_hand = []
     player_hand = []
+    dealer_score = []
+    player_score = []
 
     card = deck.pop()
     dealer_hand.append(card)
@@ -37,20 +39,51 @@ while True:
     dealer_hand.append(card)
     card = deck.pop()
     player_hand.append(card)
+    card = deck.pop()
+    dealer_hand.append(card)
+    card = deck.pop()
+    player_hand.append(card)
+    card = deck.pop()
+    dealer_hand.append(card)
+    card = deck.pop()
+    player_hand.append(card)
     print("Dealer hand:")
-    total1 = []
-    total2 = []
     for card in dealer_hand:
-        print(f"{card['rank']} of {card['suit']}")
-        total1.append(
+        print(f"{card['rank']}{card['suit']}")
+        dealer_score.append(
             card['value']
         )
-    print(sum(total1))
+    usable_aces = 0
+    ace_count = 0
+    dealer_total = sum(dealer_score)
+    for card in dealer_hand:
+        if card['rank'] == "A":
+            usable_aces += 1
+            ace_count += 1
+            if dealer_total > 21:
+                dealer_total -= 10
+                usable_aces -= 1
+                continue
+    print(dealer_total)
+    print(usable_aces)
+    print(ace_count) #make ace logic a function and rework how scoring is handled
     print("\nYour hand:")
     for card in player_hand:
-        print(f"{card['rank']} of {card['suit']}")
-        total2.append(
+        print(f"{card['rank']}{card['suit']}")
+        player_score.append(
             card['value']
         )
-    print(sum(total2))
+    found = False
+    for card in player_hand:
+        player_total = sum(player_score)
+        if card['rank'] == "A":
+            found = True
+        if found:
+            if sum(player_score) > 13:
+                player_total = sum(player_score) - 10
+                continue
+        if not found:
+            continue
+    print(player_total)
+
     break
